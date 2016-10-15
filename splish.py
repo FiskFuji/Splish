bf8e534c96a6f16e15fc4aaeb8579b016d7b4421
KirK_CW <kworley@csumb.edu>
2016-10-14 21:10:22 -0700

    Added first commit and basis of project. Splish.py

---
+++ splish.py
@@ -0,0 +1,24 @@
+#==============================================================================#
+#                           Untitled Minigame                                  #
+#==============================================================================#
+#                                                                              #
+#   Title:      Untitled Minigame -- "Splish"                                  #
+#                                                                              #
+#   Author:     Kirk Worley                                                    #
+#                                                                              #
+#   Dates:      ------------                                                   #
+#               10.14.2016:     Started project.                               #
+#               ---                                                            #
+#               ---                                                            #
+#                                                                              #
+#   Abstract:   Minigame using Python 2.7.9 and PyGame 1.9.1, to become more   #
+#               comfortable using PyGame classes and methods; as well as to    #
+#               attempt to use OOP styles, as well as new and advanced         #
+#               Python programming techniques.                                 #
+#                                                                              #
+#   GitHub:     www.github.com/KirkWorl                                        #
+#                                                                              #
+#------------------------------------------------------------------------------#
+version = 1.0   #   Versioning Purposes                                        #
+#---------------#--------------------------------------------------------------#
+#==============================================================================#
