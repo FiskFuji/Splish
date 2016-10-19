37824f32d09b067b6533416ebed1714d2ce27ebb
KirK_CW <kworley@csumb.edu>
2016-10-19 08:46:11 -0700

    Created branch for work on the Board class. Temporary commit for working reasons. Began basis of Board class with minimal field and valid board template development.

--- splish.py
+++ splish.py
@@ -1,8 +1,8 @@
 #==============================================================================#
-#                           Untitled Minigame                                  #
+#                                  "Splish"                                    #
 #==============================================================================#
 #                                                                              #
-#   Title:      Untitled Minigame -- "Splish"                                  #
+#   Title:      "Splish"                                                       #
 #                                                                              #
 #   Author:     Kirk Worley                                                    #
 #                                                                              #
@@ -18,7 +18,139 @@
 #                                                                              #
 #   GitHub:     www.github.com/KirkWorl                                        #
 #                                                                              #
-#------------------------------------------------------------------------------#
+#:::::::::::::::#--------------------------------------------------------------#
 version = 1.0   #   Versioning Purposes                                        #
-#---------------#--------------------------------------------------------------#
+#:::::::::::::::#--------------------------------------------------------------#
 #==============================================================================#
+
+#===BEGIN PROGRAM==============================================================#
+#---Import Modules---------------------------------------------------------#
+import random as rnd
+import pygame as pgm
+import sys
+
+#------Splish Class------------------------------------------------------------#
+#   This class is the game. It contains multiple other classes needed to       #
+#   run the game.                                                              #
+#---------Components:----------------------------------------------------------#
+#
+#
+#
+#
+#
+#------------------------------------------------------------------------------#
+class Splish:
+
+    #---Board Class------------------------------------------------------------#
+    #   This class contains all the workings of the board that the game is     #
+    #   played on.                                                             #
+    #------Components:---------------------------------------------------------#
+    # __init__ : 
+    #
+    #
+    #--------------------------------------------------------------------------#
+    class Board:
+
+        #---Initialize Definition---#
+        def __init__(self):
+            self.field = []
+            self.fieldPos = []
+            self.ammo = 24
+            self.boards = {
+                1: [". . . . . . . .",
+                    ". 2 . . . 3 . .",
+                    ". 2 . . . 3 . .",
+                    ". . . . . 3 . .",
+                    ". . . . . . . .",
+                    ". . . 4 4 4 4 .",
+                    ". . . . . . . .",
+                    ". . . . . . . ."],
+  
+                2: [". . . . . . . .",
+                    ". . 2 2 . . . .",
+                    ". . . . . . . .",
+                    ". . . . . . . .",
+                    ". 4 . . . . 3 .",
+                    ". 4 . . . . 3 .",
+                    ". 4 . . . . 3 .",
+                    ". 4 . . . . . ."],
+
+                3: [". . . . . . . .",
+                    ". . . . . . . .",
+                    ". 4 4 4 4 . . .",
+                    ". . . . . . . .",
+                    ". . . . . . . .",
+                    ". . . . . 3 3 3",
+                    ". . . . . . 2 .",
+                    ". . . . . . 2 ."]
+                }
+
+            for i in range(0, 7):
+                self.fieldPos.append([0, 0, 0, 0, 0, 0, 0, 0])
+                    
+
+        #---CreateBoard Definition---#
+        def createBoard(self):
+            self.field = self.boards[rnd.randint(1, len(self.boards))]
+            self.col = 0
+            self.row = 0
+            
+            for line in self.field:
+
+                if not(isinstance(line, str)):
+                    raise ValueError("The chosen board contained an element in which the row was not a string!")
+                    break
+                
+                for char in line:
+
+                    if(char == ' '):
+                        continue
+
+                    elif(char == '.'):
+                        self.fieldPos[self.row][self.col] = 0
+                        self.col += 1
+
+                    elif(char in '2 3 4' and char != ' '):
+                        self.fieldPos[self.row][self.col] = int(char)
+                        self.col += 1
+
+                self.row += 1
+
+
+
+
+x = Splish.Board()
+x.createBoard()
+
+print x.fieldPos
+print x.field
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
